96fb2dea7cb1bb2a46fbee3b41fbbf2a2b941bc2
Hudson Duarte <hudsoneeto@outlook.com>
2021-04-25 10:53:04 -0300

    feat: utilizando Link inves de a

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,5 @@
 import Button from "../../components/Button";
+import { Link } from 'react-router-dom';
 import { Container, Banner, BannerContent, BannerImage } from "./styles";
 
 function Main() {
@@ -8,9 +9,9 @@ function Main() {
                 <BannerContent>
                     <h1>Nome do Projeto</h1>
                     <p>Liquorice dessert cake ice cream gummi bears lemon drops croissant ice cream. Soufflé tootsie roll caramels jelly beans caramels cake.</p>
-                    <a href="/login">
+                    <Link to="/login">
                         <Button title="Começar" background="#01161E" backgroundHover="#33444b" />
-                    </a>
+                    </Link>
                 </BannerContent>
                 <BannerImage>
                     <img src="https://picsum.photos/500" alt="Logo Projeto" />
