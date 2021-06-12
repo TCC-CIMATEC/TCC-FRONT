358498f55a2dadd379e617ae0a3498e7c096be23
Hudson Duarte <hudsoneeto@outlook.com>
2021-06-11 21:50:33 -0300

    feat: integrando listar turmas

--- src/pages/Dashboard/Professor/index.tsx
+++ src/pages/Dashboard/Professor/index.tsx
@@ -4,21 +4,46 @@ import CardHorizontalProfessor from '../../../components/Card-horizontal-profess
 import { ButtonDashBoard } from '../../../components/Button';
 import DashboardProfessor from '../../../components/Dashboard';
 import { CardList } from '../../../components/Card-horizontal-professor/styles';
+import { useState, useEffect } from 'react';
+import api from '../../../services/api';
 
+interface Classes {
+  turma: {
+    id: number;
+    nome: string;
+    professor: {};
+    codigoTurma: string;
+    alunos: []
+  }
+}
 
 export function Professor() {
+  const [classes, setClasses] = useState<Classes[]>([]);
+
+  async function getClasses(){
+    await api.get('turma/').then((response) => {
+      setClasses(response.data);
+    }
+    );
+  }
+
+  useEffect(() => {
+    getClasses();
+  }, [])
+  
   return (
     <Container className="container-aluno">
       <DashboardProfessor>
         <Content className="content-aluno">
           <h1>Turmas</h1>
           <ProfessorContent>
-            <CardList>
-              <CardHorizontalProfessor img={"https://picsum.photos/300/200"} title={"Turma A (manhã)"} description={"Turma do 1º período"} />
-              <CardHorizontalProfessor img={"https://picsum.photos/300/200"} title={"Turma B (tarde)"} description={"Turma do 2º período"} />
-              <CardHorizontalProfessor img={"https://picsum.photos/300/200"} title={"Turma C (noite)"} description={"Turma do 1º período"} />
-              <CardHorizontalProfessor img={"https://picsum.photos/300/200"} title={"Turma C (noite)"} description={"Turma do 1º período"} />
-            </CardList>
+              {classes && (
+                <CardList>
+                  {classes.map(({turma}: Classes) => (
+                <CardHorizontalProfessor img={"https://picsum.photos/300/200"} title={turma.nome} description={turma.codigoTurma} />
+                ))}
+              </CardList>
+              )}
           </ProfessorContent>
           <div className="btn-content">
             <ButtonDashBoard title="Nova turma" background="#659157" />
